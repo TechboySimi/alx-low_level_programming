08758f5cba3a0e6ab9b865b20f7bb9b664e35afe
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-02 23:37:57 -0700

    Size is not gradeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	char c;
-	int i;
-	double d;
+	int a;
+	long int b;
+	long long int c;
+	char d;
 	float f;
-	long double lf;
 
-	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
-	printf("The size of a int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of a f is: %lu.\n", (unsigned long)sizeof(f));
-	printf("The size of a long double is: %lu.\n", (unsigned long)sizeof(lf));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
