2a7eba6c5818b769718f7b7f58452aa476a28aa1
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-02 21:23:37 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main -nEntry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,15 +3,17 @@
 int main(void)
 {
 
+	char c;
 	int i;
 	double d;
-	char c;
 	float f;
+	long double lf;
 
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
+	printf("The size of a char is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of a int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of a C is: %lu.\n", (unsigned long)sizeof(c));
 	printf("The size of a f is: %lu.\n", (unsigned long)sizeof(f));
+	printf("The size of a long double is: %lu.\n", (signed long) size of (lf));
 
 	return (0);
 }
