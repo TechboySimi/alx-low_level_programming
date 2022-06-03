d03ebbc59eec190fcbb1c435e9028026709689ec
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-03 02:16:51 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Prints out the last part of a quote in the standard error.
+ * main - main function in the task
  * Return: 1 if success.
- **/
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\"
