3df4473c777e98a7c8cad90d9b63a43cc804956a
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-03 22:13:18 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - print numbers from 00 to 99.
- *  (*
- *   * Return: 0 on success
+ * main - print numbers from 00 to 99.
+ *
+ * Return: 0 on success
 */
 int main(void)
 {
@@ -17,7 +17,7 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
-				if (i == '8' && i == '9')
+				if (i == '8' && j == '9')
 				{
 					putchar('\n');
 				}
