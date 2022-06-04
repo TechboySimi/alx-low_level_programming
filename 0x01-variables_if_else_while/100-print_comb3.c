92f57bb5abc3a782221e0a81316268eb9d8a30a4
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-03 21:57:38 -0700

    Inventing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,16 +13,19 @@ int main(void)
 	{
 		while (j <= '9')
 		{
-			putchar(i);
-			putchar(j);
-			if (i == '8' && i == '9')
+			if (!(i > j || i == j))
 			{
-				putchar('\n');
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				if (i == '8' && i == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 			j++;
 		}
