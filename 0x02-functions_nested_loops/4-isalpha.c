92fc52008be3bf8d77c639beafdfad2267f2805a
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-08 04:16:12 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,10 +8,9 @@
 
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'z')
-		if (c <= 'Z' || c <= 'a')
-		{
-			return (1);
-		}
-	return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else 
+		return (0);
+
 }
