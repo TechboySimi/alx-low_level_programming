3db7696bc2fb66579859b96e999129bbc8f8c8aa
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-08 02:34:12 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,18 @@
 
 /**
  * main - prints _putchar
+ *
  * Return: 0 on success.
 */
 int main(void)
 {
-	char c[] = "putchar";
+	char c[] = "_putchar";
 	int i;
 
 	for (i = 0; i < 8; i++)
 	{
 		putchar(c[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
