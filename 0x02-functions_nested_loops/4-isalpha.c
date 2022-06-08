f9a21f20bf854626c1fca434bd5e728ab7e7f795
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-08 03:49:01 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main h"
+
+/**
+ * isalpha - checks for alphabetic character
+ *
+ * Return: 1 if letter, else 0
+*/
+
+int _isalpha(int c);
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 132))
+		return (1);
+	else
+		return (0);
+	return (0);
+}
