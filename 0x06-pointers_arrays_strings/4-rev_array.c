3b6227f6c8b69736f035a1f07e958d41ac60dc51
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-16 16:29:54 -0700

    paranoid in reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,7 +4,7 @@
  * reverse_array - a function that reverse an array.
  *
  * @a: an input array
- * @n: number of elements of array 
+ * @n: number of elements of array
  * Return: Nothing
  */
 void reverse_array(int *a, int n)
