aabe3f4b8d4ebe60b10cbfed956b729cb213167a
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-20 12:16:04 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  * @s: location to fill
  * @b: char to fill location with
  * @n: number of bytes to fill
- * Return: returns pointer to location filled
+ * Return: returns poiter to location filled
  */
 char *_memset(char *s, char b, unsigned int n)
 {
