47f0ed282db19849f2a742aaa17620d8773bdc88
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-24 18:35:27 -0700

    This is static library

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -nEntry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
