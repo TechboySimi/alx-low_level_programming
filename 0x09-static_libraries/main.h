4d101fe73c1a037ece1daaddfd263997372d4fa3
TechboySimi <similoluwaoyebajo@gmail.com>
2022-06-24 20:05:58 -0700

    static

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifdef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,6 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#ifndef MAIN_H
-#define MAIN_H
-#endif//MAIN_H
+#endif
