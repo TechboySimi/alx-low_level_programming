6f5f25d1565963c8c7ab2d6ced7a355c77334306
Similoluwa Oyebajo <similoluwaoyebajo@gmail.com>
2022-07-19 09:56:40 -0700

    singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -50,7 +50,7 @@ size_t print_listint_safe(const listint_t *head)
 
 		while (add->next != NULL)
 		{
-			add = add=>next;
+			add = add->next;
 			if (head == add->p)
 			{
 				printf("-> [%p] %d\n", (void *)head, head->n);
