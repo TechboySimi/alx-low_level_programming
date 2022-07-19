24ff218d2054d436efbc0381559bd28e65052433
Similoluwa Oyebajo <similoluwaoyebajo@gmail.com>
2022-07-19 08:46:08 -0700

    singly linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,8 +10,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-
-
+	
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
@@ -20,6 +19,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
+	
 	return (*head);
 }
