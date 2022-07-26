b447fc63d40db0fbcda1e3e81c867b933fd48b8e
Similoluwa Oyebajo <similoluwaoyebajo@gmail.com>
2022-07-25 18:31:05 -0700

    hfge

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
