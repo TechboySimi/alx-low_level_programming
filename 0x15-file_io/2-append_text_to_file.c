ea155546d0ab0f01b7c056feed4ea03ef1687ed0
Similoluwa Oyebajo <similoluwaoyebajo@gmail.com>
2022-07-25 17:31:57 -0700

    Speak

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NUL)
-		retrun (1);
+		return (1);
 
 	whiel (text_content[lenght])
 		lenght++;
